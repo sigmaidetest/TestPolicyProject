958626d6dec37fead891b23a2f374c6fc4c4b0bd
sigmaidetest <35022586+sigmaidetest@users.noreply.github.com>
2018-06-05 09:24:45 +0800

    initial commit

--- TestPolicyProject/lambda.js
+++ TestPolicyProject/lambda.js
@@ -1,38 +1,15 @@
 let AWS = require('aws-sdk');
 exports.handler = function(event, context, callback) {
-	codestar.listProjects({
-    maxResults: 0,
-    nextToken: 'STRING_VALUE'
-}).promise()
-  .then(data => {
-      console.log('Response -> data:', data);
-      let response = {
+	let response = {
           'statusCode': 200,
           'headers': {
               'Access-Control-Allow-Origin': '*',
               'Content-Type': 'application/json'
           },
           'body': JSON.stringify({
-              'Data': data
+              'Data': 'Hello'
           }),
           'isBase64Encoded': false
       };
       callback(null, response);
-  })
-  .catch(err => {
-      console.log('Response -> error:', err);
-      let response = {
-          'statusCode': err.statusCode,
-          'headers': {
-              'Access-Control-Allow-Origin': '*',
-              'Content-Type': 'application/json'
-          },
-          'body': JSON.stringify({
-              'Code': err.code,
-              'Message': err.message
-          }),
-          'isBase64Encoded': false
-      };
-      callback(null, response);
-  });
 }
